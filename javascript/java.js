78b2462468a5588b0d838f06541883c639be9040
Emilyymm <maldonado.E055565@stu.sanjac.edu>
2026-04-21 13:27:59 -0500

    Update java.js

--- javascript/java.js
+++ javascript/java.js
@@ -1,1 +1,10 @@
+      document.getElementById('year').textContent = new Date().getFullYear();
 
+      const slides = document.querySelectorAll('.slideshow img');
+      let current = 0;
+      function showNextSlide() {
+          slides[current].classList.remove('active');
+          current = (current + 1) % slides.length;
+          slides[current].classList.add('active');
+      }
+      setInterval(showNextSlide, 4000);
