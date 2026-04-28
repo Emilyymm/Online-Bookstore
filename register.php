57c45717ffb6e0f511c33d5be9a5ede3d4a0e842
Emilyymm <maldonado.E055565@stu.sanjac.edu>
2026-04-28 01:10:55 -0500

    Create register.php

---
+++ register.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html><head><title>Register</title><link rel="stylesheet" href="css/style.css"></head>
+<body>
+<form class="form">
+<h2>Register</h2>
+<input type="text" placeholder="Username">
+<input type="email" placeholder="Email">
+<input type="password" placeholder="Password">
+<button>Create Account</button>
+</form>
+</body></html>
