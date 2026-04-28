6e1411d4603be2d3ac84596e089c678857b4d50e
Emilyymm <maldonado.E055565@stu.sanjac.edu>
2026-04-28 01:08:10 -0500

    Update books.php

--- books.php
+++ books.php
@@ -4,40 +4,12 @@
 <title>Books</title>
 <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
-
-<header>
-<h1>Book List</h1>
-</header>
-
-<section class="books-page">
-
-<div class="book">
-
-<img src="images/placeholder.png">
-
-<h3>Book Title</h3>
-
-<p>Author Name</p>
-
-<a href="add_review.php">Leave Review</a>
-
-</div>
-
-<div class="book">
-
-<img src="images/placeholder.png">
-
-<h3>Book Title</h3>
-
-<p>Author Name</p>
-
-<a href="add_review.php">Leave Review</a>
-
-</div>
-
+<header><h1>All Books</h1></header>
+<section class="grid">
+<div class="book">Placeholder Book</div>
+<div class="book">Placeholder Book</div>
+<div class="book">Placeholder Book</div>
 </section>
-
 </body>
 </html>
